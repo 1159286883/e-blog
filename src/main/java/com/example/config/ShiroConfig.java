9d69ede20d95eed1e796253473e12b1ba7b029ce
魏凌云 <1159286883@qq.com>
2021-05-28 08:21:38 +0800

    1

--- src/main/java/com/example/config/ShiroConfig.java
+++ src/main/java/com/example/config/ShiroConfig.java
@@ -42,7 +42,28 @@ public class ShiroConfig {
         filterFactoryBean.setFilters(MapUtil.of("auth", authFilter()));
 
         Map<String, String> hashMap = new LinkedHashMap<>();
+        hashMap.put("/res/**", "anon");
 
+        hashMap.put("/user/home", "auth");
+        hashMap.put("/user/set", "auth");
+        hashMap.put("/user/upload", "auth");
+        hashMap.put("/user/index", "auth");
+        hashMap.put("/user/public", "auth");
+        hashMap.put("/user/collection", "auth");
+        hashMap.put("/user/mess", "auth");
+        hashMap.put("/msg/remove/", "auth");
+        hashMap.put("/message/nums/", "auth");
+
+        hashMap.put("/collection/remove/", "auth");
+        hashMap.put("/collection/find/", "auth");
+        hashMap.put("/collection/add/", "auth");
+
+        hashMap.put("/post/edit", "auth");
+        hashMap.put("/post/submit", "auth");
+        hashMap.put("/post/delete", "auth");
+        hashMap.put("/post/reply/", "auth");
+
+        hashMap.put("/websocket", "anon");
 
         hashMap.put("/login", "anon");
         filterFactoryBean.setFilterChainDefinitionMap(hashMap);
